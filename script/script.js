549358719bcd66b72aef0bfd7f7ec59329142b95
externalsip <93773873+externalsip@users.noreply.github.com>
2023-04-15 15:49:51 -0400

    stuffagain

--- script/script.js
+++ script/script.js
@@ -4,5 +4,8 @@ const chapitre2 = document.getElementById("chapitre2");
 gsap.fromTo(circle, {y: 5, opacity: 0}, {y: 90, opacity: 1, repeat: -1, duration: 2.5, ease: "sine inOut"})
 
 gsap.timeline({scrollTrigger: {
-    
+    pin:true,
+    scrub:0.5,
+    trigger:chapitre2,
 }})
+.fromTo("#buidling1", {y:"100vh", x:"100vw"}, {y:"bottom", x:"0%"})
