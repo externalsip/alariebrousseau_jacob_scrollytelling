064232b2bb8e4f5e2af5184f75094ae1dde2e519
externalsip <93773873+externalsip@users.noreply.github.com>
2023-04-15 19:47:25 -0400

    p2.2b part 2

--- script/script.js
+++ script/script.js
@@ -1,11 +1,29 @@
+gsap.registerPlugin(ScrollTrigger);
+
 const circle = document.querySelector(".circle");
 const chapitre2 = document.getElementById("chapitre2");
+const buildingsArr = document.querySelectorAll(".building");
 
 gsap.fromTo(circle, {y: 5, opacity: 0}, {y: 90, opacity: 1, repeat: -1, duration: 2.5, ease: "sine inOut"})
 
 gsap.timeline({scrollTrigger: {
     pin:true,
-    scrub:0.5,
+    scrub:true,
     trigger:chapitre2,
+    markers:true,
+    end:"500% top"
 }})
-.fromTo("#buidling1", {y:"100vh", x:"100vw"}, {y:"bottom", x:"0%"})
+.fromTo("#building1", {y:"100%", x:"100vw"}, {y:"0", duration: 2, ease: "sine.out"})
+.to("#building1", {x:"-20vw", duration:5, ease:"none"}, "<0.1")
+.fromTo("#building2", {y:"100%", x:"100vw"}, {y:"0", duration: 2, ease: "sine.out"}, "-=5")
+.to("#building2", {x:"-20vw", duration:5, ease:"none"}, "<0.1",)
+.fromTo("#building3", {y:"100%", x:"100vw"}, {y:"0", duration: 2, ease: "sine.out"}, "-=4.8")
+.to("#building3", {x:"-20vw", duration:5, ease:"none"}, "<0.1")
+.fromTo("#building4", {y:"100%", x:"100vw"}, {y:"0", duration: 2, ease: "sine.out"}, "-=4.9")
+.to("#building4", {x:"-20vw", duration:5, ease:"none"}, "<0.1")
+.fromTo("#building5", {y:"100%", x:"100vw"}, {y:"0", duration: 2, ease: "sine.out"}, "-=4.5")
+.to("#building5", {x:"-20vw", duration:5, ease:"none"}, "<0.1")
+.fromTo("#building6", {y:"100%", x:"100vw"}, {y:"0", duration: 2, ease: "sine.out"}, "-=4.7")
+.to("#building6", {x:"-20vw", duration:5, ease:"none"}, "<0.1")
+.fromTo("#building7", {y:"100%", x:"100vw"}, {y:"0", duration: 2, ease: "sine.out"}, "-=5.4")
+.to("#building7", {x:"-20vw", duration:5, ease:"none"}, "<0.1")
