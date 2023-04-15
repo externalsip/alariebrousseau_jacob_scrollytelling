c71b9c7702e60f7d198b1078add8613465360964
externalsip <93773873+externalsip@users.noreply.github.com>
2023-04-15 15:27:05 -0400

    t2.2b part1

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
 const circle = document.querySelector(".circle");
+const chapitre2 = document.getElementById("chapitre2");
 
 gsap.fromTo(circle, {y: 5, opacity: 0}, {y: 90, opacity: 1, repeat: -1, duration: 2.5, ease: "sine inOut"})
+
+gsap.timeline({scrollTrigger})
